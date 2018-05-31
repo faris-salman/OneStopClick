2796322522f9b45cc8ee40e8077ffab8f146de3f
M Salman Al Farisi <MuhammadSalman.AlFarisi@mitrais.com>
2018-05-31 15:54:35 +0700

    delete some unecessary files
    added missing icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{asset('css/all.css')}}">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>react-laravel-with-jwt-auth</title>
+        <title>OneStopCLick</title>
 
     </head>
     <body>
