da1e24dd87b230ec57245480275a97202f54ba13
patriciaasmoro <stellamaris.patriciaasmoro@mitrais.com>
2018-06-04 10:09:19 +0700

    tes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{asset('css/all.css')}}">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>OneStopCLick</title>
+        <title>OneStopCLick lalala</title>
 
     </head>
     <body>
