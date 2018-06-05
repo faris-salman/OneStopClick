852d83e72a8e1fd727046faa0f6ce69d9a04488f
patriciaasmoro <stellamaris.patriciaasmoro@mitrais.com>
2018-06-05 16:20:12 +0700

    Moved controllers to Api folder;
    Create Owner table;
    Create table's normalisation for Owner-Product and Category-Product;

---
+++ app/Models/Owner.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Owner extends Model
+{
+    // Add some more fields later.
+    protected $fillable = ['name'];
+}
--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Category;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class CategoriesTableSeeder extends Seeder
@@ -12,13 +13,13 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        $faker = \Faker\Factory::create();
- 
-        // Create 10 category records
-        for ($i = 0; $i < 10; $i++) {
-            Category::create([
-                'name' => $faker->lastName
-            ]);
-        }
+        $now = Carbon::now()->toDateTimeString();
+
+        Category::insert([
+            ['name' => 'Movie', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Music', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Game', 'created_at' => $now, 'updated_at' => $now],
+            ['name' => 'Anime', 'created_at' => $now, 'updated_at' => $now],
+        ]);
     }
 }
